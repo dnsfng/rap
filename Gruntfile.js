a5d8ff4e6ee43ab303910b6a45899ca618b298fd
dnsfng <Work@dnsfng.local>
2014-10-16 13:17:23 +0200

    Add node packages config

---
+++ Gruntfile.js
@@ -0,0 +1,112 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+      '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+      '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+      '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+      ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    concat: {
+      options: {
+        banner: '<%= banner %>',
+        stripBanners: true
+      },
+      dist: {
+        src: ['lib/<%= pkg.name %>.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '<%= banner %>'
+      },
+      dist: {
+        src: '<%= concat.dist.dest %>',
+        dest: 'dist/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        node:true, // add to remove node undefined error such as 'require' function from stylus config
+        globals: {
+          jQuery: true
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      },
+      css: {
+        files: ['dev/stylesheets/*.styl'],
+        tasks: ['stylus'],
+        options: {
+          // Start a live reload server on the default port 35729
+          livereload: true,
+        },
+      }
+    },
+    stylus: { 
+      compile: {
+        options: {
+          'include css': true,
+          paths: ['dev/stylesheets/**'],
+          urlfunc: 'embedurl',
+          limenos: true,
+          use: [
+            function () {
+                      return require('autoprefixer-stylus')({ browsers: 'last 2 version' });
+                    },
+            //require('csso-stylus') //don't need it anymore
+          ]
+        },
+        files: {
+          'public/_assets/c/app.min.css': 'dev/stylesheets/app.styl'
+        }
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-stylus'); // + autoprefixer-stylus // + csso-stylus
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify', 'stylus']);
+
+};

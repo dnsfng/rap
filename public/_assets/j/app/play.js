b5779e30a1a36125141daf7c76ca0888977edd9c
dnsfng <Work@dnsfng.local>
2014-10-20 23:14:44 +0200

    Add javascript fix for vw support on Safari

--- public/_assets/j/app/play.js
+++ public/_assets/j/app/play.js
@@ -23,4 +23,19 @@ $(document).ready(function(){
 
   $('.js--open-modal').click(getReference);
 
+
+  // Repaint resize hack on vw unit (Safari)
+
+  var causeRepaintsOn = $("section");
+
+  $(window).resize(function() {
+    causeRepaintsOn.css("z-index", 1);
+  });
+
+  // Found a css solution
+  // $('.js--ref--item').hover(function(){
+  //     console.log('ok');
+  //     $(this).children('a').css({'background-color': 'yellow'});
+  // });
+
 });

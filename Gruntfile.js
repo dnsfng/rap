780be450768092508bca901210df347855d52caf
dnsfng <Work@dnsfng.local>
2014-10-29 11:05:42 +0100

    Add & update modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -110,7 +110,77 @@ module.exports = function(grunt) {
         src: 'dev/scripts/app/*',
         dest: 'public/_assets/j/app/',
         flatten: true
+      }
+    },
+    wiredep: {
+      task: {
+        src: ['craft/templates/_layout.html']
       },
+      options: {
+      }
+    },
+    modernizr: {
+
+        dist: {
+            // [REQUIRED] Path to the build you're using for development.
+            "devFile" : "bower_components/modernizr/modernizr.js",
+
+            // Path to save out the built file.
+            "outputFile" : "public/_assets/j/vendor/modernizr-custom.js",
+
+            // Based on default settings on http://modernizr.com/download/
+            "extra" : {
+                "shiv" : true,
+                "printshiv" : false,
+                "load" : true,
+                "mq" : false,
+                "cssclasses" : true
+            },
+
+            // Based on default settings on http://modernizr.com/download/
+            "extensibility" : {
+                "addtest" : false,
+                "prefixed" : false,
+                "teststyles" : false,
+                "testprops" : false,
+                "testallprops" : false,
+                "hasevents" : false,
+                "prefixes" : false,
+                "domprefixes" : false,
+                "cssclassprefix": ""
+            },
+
+            // By default, source is uglified before saving
+            "uglify" : true,
+
+            // Define any tests you want to implicitly include.
+            "tests" : [],
+
+            // By default, this task will crawl your project for references to Modernizr tests.
+            // Set to false to disable.
+            "parseFiles" : true,
+
+            // When parseFiles = true, this task will crawl all *.js, *.css, *.scss and *.sass files,
+            // except files that are in node_modules/.
+            // You can override this by defining a "files" array below.
+            "files" : {
+                "src": [
+                  'dev/scripts/app/*.js',
+                  'dev/stylesheets/*.styl',
+                  ]
+            },
+
+            // This handler will be passed an array of all the test names passed to the Modernizr API, and will run after the API call has returned
+            // "handler": function (tests) {},
+
+            // When parseFiles = true, matchCommunityTests = true will attempt to
+            // match user-contributed tests.
+            "matchCommunityTests" : false,
+
+            // Have custom Modernizr tests? Add paths to their location here.
+            "customTests" : []
+        }
+
     }
   });
 
@@ -122,6 +192,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-stylus'); // + autoprefixer-stylus // + csso-stylus
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-wiredep');
+  grunt.loadNpmTasks("grunt-modernizr");
 
   // Default task.
   //grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify', 'stylus']);
